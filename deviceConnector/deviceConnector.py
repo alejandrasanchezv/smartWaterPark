add1750a5a9e0075192d84b6c4e78588b41b439f
alejandrasanchezv <maria.a.sanchez11@gmail.com>
2023-11-01 18:27:40 +0100

    deviceConnector

--- deviceConnector/deviceConnector.py
+++ deviceConnector/deviceConnector.py
@@ -5,35 +5,74 @@ import json
 #import cherrypy
 
 from mqttClass import *
+from devices import *
 
 database = "devices.json"
+counterID = 0
+airID = 0
+waterLevelID = 0
+phID = 0
+airpumpID = 0
+valveID = 0
+chlorineValveID = 0
+lightsID = 0
+fansID = 0
+callMaintID = 0
 
-class publisher(object):
-  def __init__(self):
-    global database, usrID, rideID
+class Publisher(object):
+  def __init__(self, client):
+    global database, counterID, airID, waterLevelID, phID,\
+      airpumpID, valveID, chlorineValveID, lightsID, fansID, callMaintID
+    
+    with open(database, "r") as file:
+      db = json.load(file)
 
-    self.sensors = db["devices"]["sensors"]
-    self.actuators = db["devices"]["actuators"]
+    usrID = db["userID"]
+    rideID = db["rideID"]
+    self.topic = "smartWaterPark/devConnector/user_" + str(usrID) + "/ride_" + str(rideID) + "/#"
+    self.client = client + str(usrID)
+    self.mqttClient = ClientMQTT(self.client, [self.topic],onMessageReceived=onMsgReceived)
+
+    self.sensorsList = db["devices"]["sensors"]
+    self.actuatorsList = db["devices"]["actuators"]
     self.strategies = db["strategies"]
+    self.sensors = []
+    self.actuators = []
 
+    for sensor in self.sensorsList:
+      if sensor == "counterRides":
+        self.sensors.append(Sensor(counterID, sensor))
+      elif sensor == "airWeight":
+        self.sensors.append(Sensor(airID, sensor))
+      elif sensor == "waterLevel":
+        self.sensors.append(Sensor(waterLevelID, sensor))
+      elif sensor == "phSensor":
+        self.sensors.append(Sensor(phID, sensor))
 
-  
+    # Actuator are always initialized as off
+    for actuator in self.actuatorsList: 
+      if actuator == "airPump":
+        self.actuators.append(Actuator(airpumpID, False, actuator))
+      elif actuator == "maintenanceCall":
+        self.actuators.append(Actuator(callMaintID, False, actuator))
+      elif actuator == "waterValve":
+        self.actuators.append(Actuator(valveID, False, actuator))
+      elif actuator == "chlorineValve":
+        self.actuators.append(Actuator(chlorineValveID, False, actuator))
+      elif actuator == "lights":
+        self.actuators.append(Actuator(lightsID, False, actuator))
+      elif actuator == "fans":
+        self.actuators.append(Actuator(fansID, False, actuator))
 
-def onMsgReceived(device1, userdata, msg):
+  def onMsgReceived(device1, userdata, msg):
     print(f"Message received. Topic:{msg.topic}, QoS:{msg.qos}s, Message:{msg.payload}")
 
-with open(database, "r") as file:
-  db = json.load(file)
 
-usrID = db["userID"]
-rideID = db["rideID"]
 
 
 if __name__ == "__main__":
-  topic = "smartWaterPark/devConnector/user_" + str(usrID) + "/ride_" + str(rideID) + "/#"
-  client = "devConnector" + str(usrID)
-  devMqtt = ClientMQTT(client, [topic],onMessageReceived=onMsgReceived)
 
+  devMqtt = Publisher("devConnector")
   devMqtt.start()
 	
   while True:
--- deviceConnector/devices.py
+++ deviceConnector/devices.py
@@ -1,28 +1,52 @@
 import time
 import json
+from random import uniform
 import paho.mqtt.client as PahoMQTT
 
 from mqttClass import *
-
-class Weather(object):
-    pass
 
 class Actuator(object):
     def __init__(self, id, state, type) -> None:
         self.id = id
         self.state = state
         self.type = type
-    
+        if self.type == "airPump" or self.type == "waterValve" or self.type == "chlorineValve":
+            self.value = 0
+
     def actuatorOn(self):
         self.state = True
     
     def actuatorOff(self):
         self.state = False
 
+    def setValue(self, value):
+        self.value = value
+        if self.value == 0:
+            self.state = False
+        else:
+            self.state = True
+
 class Sensor(object):
     def __init__(self, id, type) -> None:
         self.id = id
         self.type = type
+        self.value = 0
+
+    def readvalue(self, type):
+        if type == "counterRides":
+            self.value += uniform(10,50)
+        elif type == "airWeight":
+            self.value = uniform(10,30)
+        elif type == "waterLevel":
+            self.value = uniform(10.3,20.0)
+        elif type == "phSensor":
+            self.value = uniform(6.8,8.5) #Ideal between 7.2 amd 7.4
+        else:
+            self.value = 0
+            return 'Invalid sensor type'
+
+class Weather(object):
+    pass
 
 class Maintenance(object):
     def __init__(self, sensors, actuators):
@@ -47,6 +71,9 @@ class Maintenance(object):
                 typeSensor = sensor.type
                 typeSensor.readValue()
                 return sensor.value
+            
+    def callMaintenance(self, userid, rideid):
+        print(f'Ride with ID {rideid} registered unser user {userid}, needs maintance')
 
 class Comfort(object):
     def __init__(self, sensors, actuators):
@@ -65,10 +92,8 @@ class Comfort(object):
                 actuator.actuatorOff()
                 return f'Comfort Actuator {actuator.type} with ID {id} is off'
 
-
 def onMsgReceived(device1, userdata, msg):
     print(f"Message received. Topic:{msg.topic}, QoS:{msg.qos}s, Message:{msg.payload}")
-
 
 
 if __name__ == "__main__":
