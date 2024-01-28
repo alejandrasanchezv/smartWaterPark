4368cbeea9c41b83736ea02691c207c15ce439e6
alejandrasanchezv <maria.a.sanchez11@gmail.com>
2024-01-28 23:00:02 +0100

    funciona thingSpeak

---
+++ ThingSpeak/thingSpeakAdaptor.py
@@ -0,0 +1,240 @@
+import cherrypy
+import requests
+import time
+import json
+import paho.mqtt.client as mqtt
+
+from mqttClass import *
+
+database = "./thingSpeakDB.json"
+resCatEndpoints = "http://127.0.0.1:8080"
+url_thingspeak = "https://api.thingspeak.com/update?api_key="
+
+maintData = {
+    "isinMaint": -1,
+    "numMaint": -1,
+    "stateAlert": -1
+}
+
+class ThingSpeakAdaptor(object):
+    exposed = True
+
+    def POST(self, **params):
+        """
+        Registers a new topic
+        """
+        global database, db, thingsSpeakMqtt
+
+        input = params
+        print(params)
+
+        try:
+            usrID = input['userID']
+            rideID = input['rideID']
+        except:
+            raise cherrypy.HTTPError(400, 'Wrong input')
+        
+        topic = "smartWaterPark/thingSpeak/user/" + str(usrID) + "/ride/" + str(rideID) + "/#"
+        newTopic = {
+            "topic": topic
+        }
+
+        db["topics"].append(newTopic)
+        thingsSpeakMqtt.subscribe(topic)
+
+        with open(database, "w") as file:
+            json.dump(db, file, indent=3)
+
+        result = {
+            "userID": usrID,
+            "rideID": rideID
+        }
+
+        return result
+    
+    def DELETE(self, **params):
+        """
+        Deletes a new topic
+        """
+
+        global database, thingsSpeakMqtt
+
+        with open(database, "r") as file:
+            db = json.load(file)
+
+        input = params
+        print(params)
+
+        try:
+            usrID = input['userID']
+            rideID = input['rideID']
+        except:
+            raise cherrypy.HTTPError(400, 'Wrong input')
+        
+        index = []
+        user = str(usrID)
+        ride = str(rideID)
+        for i, topic in enumerate(db['topics']):
+            root_topic = topic['topic'].split('/')
+            if root_topic[3] == user and root_topic[5] == ride:
+                index.append(i)
+                thingsSpeakMqtt.unsubscribe(topic['topic'])
+
+        index.sort(reverse=True) #decendign order
+        for i in index:
+            db['topics'].pop(i)
+
+        with open(database, "w") as file:
+            json.dump(db, file, indent=3)
+
+        result = {
+            "userID": usrID,
+            "rideID": rideID
+        }
+
+        return result
+    
+class ThingSpeakmqtt(object):
+    def __init__(self) -> None:
+        pass
+
+    def onMsgReceived(device1, userdata, msg):
+        print(f"Message received. Topic:{msg.topic}, QoS:{msg.qos}s, Message:{msg.payload}")
+        data = json.loads(msg.payload)
+        topic = msg.topic
+
+        sendThingSpeak(topic, data)
+
+def sendThingSpeak(topic, data):
+    """
+    Sends the information received from 
+    a MQTT topic to Thingspeak using REST (post)
+    """
+
+    print(f'sendThingSpeak entered with topic: "{str(topic)}" and data: "{str(data)}"')
+    global database
+
+    with open(database, "r") as file:
+        db = json.load(file)
+
+    userID = topic.split("/")[3]
+    print(f'userID: {userID}')
+    rideID = topic.split("/")[5]
+    print(f'rideID: {rideID}')
+    dataType = topic.split("/")[6]
+    print(f'dataType: {dataType}')
+    maintData[dataType] = data[dataType]
+
+    for user in db["users"]:
+        if user["userID"] == int(userID):
+            for ride in user["rides"]:
+                if ride["rideID"] == int(rideID):
+                    apiKey = ride["API_KEY"]
+
+                    if maintData["isinMaint"] >= 0:
+                        field_inMaint = ride["isinMaint"]
+                        RequestToThingspeak = str(url_thingspeak + apiKey + field_inMaint).format(int(maintData["isinMaint"]))
+                        #requests.post(RequestToThingspeak)
+                        #print(RequestToThingspeak)
+                    
+                    if maintData["numMaint"] >= 0:
+                        field_numMaint = ride["numMaint"]
+                        RequestToThingspeak = str(url_thingspeak + apiKey + field_numMaint).format(int(maintData["numMaint"]))
+                        #requests.post(RequestToThingspeak)
+                        #print(RequestToThingspeak)
+
+                    if maintData["stateAlert"] >= 0:
+                        field_alert = ride["stateAlert"]
+                        RequestToThingspeak = str(url_thingspeak + apiKey + field_alert).format(int(maintData["stateAlert"]))
+
+                    #RequestToThingspeak = str(url_thingspeak + apiKey + field_inMaint + field_numMaint + field_alert).format(int(maintData["isinMaint"]), int(maintData["numMaint"]), int(maintData["stateAlert"]))
+                    requests.post(RequestToThingspeak)
+                    print(RequestToThingspeak)
+
+                    maintData["isinMaint"] = -1
+                    maintData["numMaint"] = -1
+                    maintData["stateAlert"] = -1
+
+                    #RequestToThingspeak = str(url_thingspeak + apiKey + field_inMaint + field_numMaint + field_alert).format(int(maintData["isinMaint"]), int(maintData["numMaint"]), int(maintData["stateAlert"]))
+                    #requests.post(RequestToThingspeak)
+
+def postFunc():
+  global database
+
+  with open(database, "r") as file:
+    db = json.load(file)
+
+  payload = {
+    "userID": db['userID'],
+    "rideID": db['rideID'],
+  }
+
+  url = resCatEndpoints +'/thingSpeakAdaptor'
+  requests.post(url, json.dumps(payload))
+
+def getTopics():
+    """
+    Retrieves all the topics registered in the Resource Catalog
+    """
+    global database
+
+
+
+    #url = resCatEndpoints +'/thingSpeakAdaptor'
+
+
+
+
+if __name__ == "__main__":
+
+    time.sleep(10)
+
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+        }
+    }
+    cherrypy.tree.mount(ThingSpeakAdaptor(), '/dbTopic', conf)
+    cherrypy.config.update({'server.socket_host': '127.0.0.1', 'server.socket_port': 8096})
+    cherrypy.engine.start()
+
+    with open(database, "r") as file:
+        db = json.load(file)
+
+    usrID = db["userID"]
+    rideID = db["rideID"]
+    topic = "smartWaterPark/thingSpeak/user/" + str(usrID) + "/ride/" + str(rideID) + "/#"
+    client = "thingSpeak" + str(usrID)
+    thingsSpeakMqtt = ClientMQTT(client, [topic],onMessageReceived=ThingSpeakmqtt.onMsgReceived)
+    thingsSpeakMqtt.start()
+
+    exampleData = {
+        "isinMaint": 1,
+        "numMaint": 1,
+        "stateAlert": 0
+    }
+
+    time.sleep(5)
+
+    topicTest = "smartWaterPark/thingSpeak/user/" + str(usrID) + "/ride/" + str(rideID) + "/isinMaint"
+    sendThingSpeak(topicTest, exampleData)
+
+    time.sleep(15)
+
+    topicTest = "smartWaterPark/thingSpeak/user/" + str(usrID) + "/ride/" + str(rideID) + "/numMaint"
+    sendThingSpeak(topicTest, exampleData)
+
+    time.sleep(15)
+
+    topicTest = "smartWaterPark/thingSpeak/user/" + str(usrID) + "/ride/" + str(rideID) + "/stateAlert"
+    sendThingSpeak(topicTest, exampleData)
+
+    timeLimitDB = 60 # number in seconds
+    #postFunc()
+    #while True:
+    #    timeNow = time.time()
+    #    if (timeNow - timeLastDB) >= timeLimitDB:
+    #        postFunc()
+    #        timeLastDB = timeNow
+    #thingsSpeakMqtt.stop()
